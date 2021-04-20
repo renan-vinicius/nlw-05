9eec99c1057df336041a48b22e0076e7c824ef0f
Renan Vinicius <renan.ufg@gmail.com>
2021-04-20 17:27:10 -0300

    Aula 02 - Banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,10 @@
 import express from "express";
+import "./database";
+import { routes } from "./routes";
 const app = express();
+app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json({
-    message: "Olá NLW05",
-  });
-});
-
-app.post("/", (request, response) => {
-  return response.json({
-      message: "Usuário salvo com sucesso "
-    });
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log("Servidor rodando na porta 3333");
